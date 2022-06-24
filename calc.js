463a6a1154ba81f0fd00c7e252d4c218be933fe3
Neelam Nalan <80204527+neelamnalan@users.noreply.github.com>
2022-06-24 14:26:20 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,25 @@
+﻿function add()
+{
+var n1=parseFloat(document.getElementById("num1").value)
+var n2=parseFloat(document.getElementById("num2").value)
+document.getElementById("ans").innerHTML=n1+n2
+}
+function minus()
+{
+var n1=parseFloat(document.getElementById("num1").value)
+var n2=parseFloat(document.getElementById("num2").value)
+document.getElementById("ans").innerHTML=n1-n2
+}
+function multi()
+{
+var n1=parseFloat(document.getElementById("num1").value)
+var n2=parseFloat(document.getElementById("num2").value)
+document.getElementById("ans").innerHTML=n1*n2
+}
+function division()
+{
+var n1=parseFloat(document.getElementById("num1").value)
+var n2=parseFloat(document.getElementById("num2").value)
+document.getElementById("ans").innerHTML=n1/n2
+}
+
